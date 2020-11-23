9e9e5a6d73a70532900c11dfb66a055664040c08
Remy <remy.tang@telecom-paris.fr>
2020-11-23 15:05:16 +0100

    Ajout du constructeur MBox et des getteurs pour les positions (x,y)

--- src/tp04/Mbox.java
+++ src/tp04/Mbox.java
@@ -3,4 +3,12 @@ package tp04;
 public abstract class Mbox {
 	private int xPos ;
 	private int yPos ;
+	
+	public int getXPos() {
+		return xPos ;
+	}
+	
+	public int getYPos() {
+		return yPos ;
+	}
 }
