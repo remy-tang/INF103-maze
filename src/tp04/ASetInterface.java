f39dab585ec4983f498c0f7fce9107860399841d
Remy <remy.tang@telecom-paris.fr>
2020-12-11 10:26:37 +0100

    ajout isInA

--- src/tp04/ASetInterface.java
+++ src/tp04/ASetInterface.java
@@ -2,6 +2,6 @@ package tp04;
 
 public interface ASetInterface {
 	void initialise(Vertex root) ; /* Initialise A avec la racine */
-	void showVertices() ; /* Donne la liste des sommets du graphe dans A */
+	bool isInA(Vertex currentVertex) ; /* Indique si le sommet currentVertex est dans A */
 	void addVertex(Vertex newVertex) ; /* Ajoute un sommet du graphe (qui n'est pas encore ajouté) dans A */
 }
