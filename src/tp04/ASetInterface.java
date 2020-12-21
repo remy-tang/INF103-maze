495fe478d8649a39a1f46e824fb08d394848f75d
Remy <remy.tang@telecom-paris.fr>
2020-12-21 21:02:00 +0100

    Updated inferfaces

--- src/tp04/ASetInterface.java
+++ src/tp04/ASetInterface.java
@@ -1,7 +1,6 @@
 package tp04;
 
 public interface ASetInterface {
-	void initialise(Vertex root) ; 		/* Initialise A avec la racine */
 	boolean isInA(Vertex currentVertex) ; 	/* Indique si le sommet currentVertex est dans A */
 	void addVertex(Vertex newVertex) ; 	/* Ajoute un sommet du graphe (qui n'est pas encore ajouté) dans A */
 }
--- src/tp04/PiInterface.java
+++ src/tp04/PiInterface.java
@@ -2,5 +2,5 @@ package tp04;
 
 public interface PiInterface {
 	void initialisePi() ; /* pi(racine) = 0 , pour tout autre sommet x du graphe pi(x)=+infini */
-	void newPiValue(Vertex y); /* Met à jour la valeur pi(y) si celle calculée est meilleure */
+	void newPiValue(Vertex y); /* Met à jour la valeur pi(y)  */
 }
