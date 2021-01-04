72166a0c950e58c9d901cd3755e1d30bc5dd8773
Remy <remy.tang@telecom-paris.fr>
2021-01-04 16:04:22 +0100

    implemented Previous using Hashtable, and added missing functions in PreviousInterface

---
+++ src/dijkstra/Previous.java
@@ -0,0 +1,31 @@
+package dijkstra;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Previous implements PreviousInterface {
+
+	private Hashtable<VertexInterface, VertexInterface> previousTable = new Hashtable<VertexInterface, VertexInterface>();
+	
+	public void updatePrevious(VertexInterface y, VertexInterface pivot) {
+		previousTable.put(y,pivot);
+	}
+
+	public VertexInterface getValue(VertexInterface vertex) {
+		return previousTable.get(vertex);
+	}
+
+	public ArrayList<VertexInterface> getShortestPathTo(VertexInterface vertex) {
+		ArrayList<VertexInterface> vertexList = new ArrayList<VertexInterface>();
+		VertexInterface currentVertex = vertex;
+		
+		while (currentVertex.getLabel() != "DBox") {
+			vertexList.add(currentVertex);
+			currentVertex = getValue(currentVertex);
+		}
+		vertexList.add(currentVertex);
+		return vertexList;
+
+	}
+
+}
--- src/dijkstra/PreviousInterface.java
+++ src/dijkstra/PreviousInterface.java
@@ -1,5 +1,10 @@
 package dijkstra;
+import java.util.ArrayList;
 
 public interface PreviousInterface {
-	void updatePrevious(VertexInterface y, VertexInterface pivot); /* Met à jour le père dans A du sommet y */
+	public void updatePrevious(VertexInterface y, VertexInterface pivot); /* Met à jour le père dans A du sommet y */
+	
+	public VertexInterface getValue(VertexInterface vertex); /* Donne la valeur d'un sommet, null si non initialisé */
+	
+	public ArrayList<VertexInterface> getShortestPathTo(VertexInterface vertex); /* Donne le plus cours chemin de la racine au sommet vertex */
 }
