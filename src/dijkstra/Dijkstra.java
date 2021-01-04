cc9f81f1bf219f91f8556d332ad83321f6e0ce37
Remy <remy.tang@telecom-paris.fr>
2021-01-04 17:01:40 +0100

    implemented dijkstra, not yet tested

--- src/dijkstra/Dijkstra.java
+++ src/dijkstra/Dijkstra.java
@@ -1,9 +1,56 @@
 package dijkstra;
+import java.util.ArrayList;
 
 public class Dijkstra {
 	
-	private PreviousInterface dijkstra(GraphInterface g,VertexInterface r, ASetInterface a, PiInterface pi, PreviousInterface previous) {
+	private static PreviousInterface dijkstra(GraphInterface g,VertexInterface r, ASetInterface a, PiInterface pi, PreviousInterface previous) {
 		
+		return previous;
+	}
+	
+	public static PreviousInterface dijkstra(GraphInterface g, VertexInterface r) {
+		
+		ASetInterface A = new ASet();
+		PiInterface Pi = new Pi();
+		PreviousInterface previous = new Previous();
+		ArrayList<VertexInterface> vertexList = g.getAllVertices();
+		
+		int n = vertexList.size();
+		VertexInterface pivot = r;
+		VertexInterface nextPivot = r;
+		Pi.setValue(r, 0);
+		
+		for (VertexInterface vertex : vertexList) {
+			if (vertex != r)
+				Pi.setValue(vertex, Integer.MAX_VALUE);
+		}
+		
+		for (int j=1; j<n; j++) {
+			ArrayList<VertexInterface> pivotSuccessors = g.getSuccessors(pivot);
+			
+			for (VertexInterface y : pivotSuccessors) {
+				if (!A.isInA(y)) {
+					if (Pi.getValue(y) + g.getWeight(pivot, y) < Pi.getValue(y)) {
+						Pi.setValue(y, Pi.getValue(pivot) + g.getWeight(pivot, y));
+						previous.updatePrevious(y, pivot);
+					}
+				}
+			}
+			
+			int minPi = Integer.MAX_VALUE;
+			
+			for (VertexInterface vertex : vertexList) {
+				if (!A.isInA(vertex)) {
+					if (Pi.getValue(vertex) < minPi) {
+						minPi = Pi.getValue(vertex);
+						nextPivot = vertex;
+					}
+				}
+			}
+			
+			pivot = nextPivot;
+			A.addVertex(pivot);
+		}
 		return previous;
 	}
 }
