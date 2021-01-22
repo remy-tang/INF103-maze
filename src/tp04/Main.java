f195fb59096762afc0b802656cc033dfe9445d4e
Remy <remy.tang@telecom-paris.fr>
2021-01-22 12:20:19 +0100

    parameterized file names in main.java and updated the JavaDoc accordingly

--- src/tp04/Main.java
+++ src/tp04/Main.java
@@ -11,18 +11,22 @@ public class Main {
 	/**
 	 * Résout un labyrinthe et sauvegarde la solution dans un fichier texte.
 	 * Tous les fichiers sont dans le dossier data.
-	 * Le labyrinthe intial doit être nommé labyrinthe.txt.
-	 * Le labyrinthe résolu est sauvegardé dans labyrintheResolu.txt
+	 * Le labyrinthe intial doit être nommé mazeFileName.txt.
+	 * Le labyrinthe résolu est sauvegardé dans solvedMazeFileName.txt.
 	 * 
-	 * @param args arguments de main
+	 * @param args arguments de main (vide ici)
 	 */
 	public static void main(String[] args) {
 		
+		/* Paramètres */
+		String mazeFileName = "labyrinthe";
+		String solvedMazeFileName = "labyrintheResolu";
+		
 		/* Initialisation du labyrinthe */
-		int pMax = Maze.firstLineLen("labyrinthe") - 1;
-		int nMax = Maze.countLines("labyrinthe") - 1;
+		int pMax = Maze.firstLineLen(mazeFileName) - 1;
+		int nMax = Maze.countLines(mazeFileName) - 1;
 		Maze maze = new Maze(nMax, pMax);
-		try {maze.initFromTextFile("labyrinthe");}
+		try {maze.initFromTextFile(mazeFileName);}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -36,7 +40,7 @@ public class Main {
 		System.out.print(solvedMazeString);
 
 		/* Sauvegarde du labyrinthe résolu dans data/solvedLabyrinthe.txt */
-		try {Maze.saveToTextFile("labyrintheResolu", solvedMazeString);}
+		try {Maze.saveToTextFile(solvedMazeFileName, solvedMazeString);}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
